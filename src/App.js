b4e043a014a1a14963ef0077b7de2599404e6630
Andriana Ioannides <ioannidou.aa@gmail.com>
2021-04-15 14:05:46 +0100

    Added Github link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is created by A.A Ioannides and is{" "}
+        <a
+          href="https://github.com/AIoannides/react-app-weather"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
