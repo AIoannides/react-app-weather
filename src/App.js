f6f9f762bf072b84ed557adefeaf2a87742b9057
Andriana Ioannides <ioannidou.aa@gmail.com>
2021-04-15 14:18:27 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project is created by A.A Ioannides and is{" "}
-        <a
-          href="https://github.com/AIoannides/react-app-weather"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project is created by A.A Ioannides and is{" "}
+          <a
+            href="https://github.com/AIoannides/react-app-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
